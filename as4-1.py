a046181be9139a2b756f80e995cfce214d9fa3f2
saeedeshaghi77 <saeedeshaghi77@gmail.com>
2022-09-02 22:41:29 +0430

    a4.v1

--- as4-1.py
+++ as4-1.py
@@ -1,1 +1,7 @@
 # fact 6 = 6*5*4*3*2*1
+number=int(input('enter your number : '))
+sum=1
+for i in range (1,number+1):
+    sum*=i
+print('fact: ', sum)    
+
--- as4-2.py
+++ as4-2.py
@@ -1,6 +1,11 @@
 #n = input
-
-
+n=int(input('how many enter number in document : '))
+x=[]
+while len(x)!=n :
+    number = float(input('enter your number: '))
+    if number%2==0:
+        x.append(number)
+print(x)
 
 
 
