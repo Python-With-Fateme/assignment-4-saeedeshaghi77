ac7cd29026eece26f600effe80e72f345774a6dd
saeedeshaghi77 <saeedeshaghi77@gmail.com>
2022-09-06 17:03:22 +0430

    a4.v4

--- as4-1.py
+++ as4-1.py
@@ -1,7 +1,9 @@
 # fact 6 = 6*5*4*3*2*1
-number=int(input('enter your number : '))
-sum=1
-for i in range (1,number+1):
-    sum*=i
-print('fact: ', sum)    
-
+n=int(input('how many number ? : '))
+for j in range(n):
+    number=int(input(f'enter your number {j+1} : '))
+    sum=1
+    for i in range (1,number+1):
+        sum*=i
+    print('fact: ', sum)    
+    
--- as4-4.py
+++ as4-4.py
@@ -1,1 +1,24 @@
+import random
+ar=['apple','orange','bananna','potato']
+word=list(random.choice(ar))
+guess='-'*len(word)
+print(guess)
+ar=list(guess)
+counter=0
+while True:
+    char=input('Enter your guess : ')
+    for i in range(len(word)):
+        if word[i]==char:
+            ar[i]=word[i]
+            break    
+    else:        
+        counter+=1
+        print('False')    
+    if counter == 5:
+        print('looooseerrr ')
+        break
+    if ar == word:
+        print('you are Winner ')
+        break    
+    print(''.join(ar))    
 
