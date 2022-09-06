0429baf3c1abb536f43f5d37f24cba3a30d7348d
saeedeshaghi77 <saeedeshaghi77@gmail.com>
2022-09-06 17:31:11 +0430

    a4.v5

--- as4-4.py
+++ as4-4.py
@@ -4,19 +4,17 @@ word=list(random.choice(ar))
 guess='-'*len(word)
 print(guess)
 ar=list(guess)
-counter=0
+counter=len(word)
 while True:
     char=input('Enter your guess : ')
-    for i in range(len(word)):
+    for i in range(len(word)): 
         if word[i]==char:
             ar[i]=word[i]
-            break    
-    else:        
-        counter+=1
-        print('False')    
-    if counter == 5:
-        print('looooseerrr ')
-        break
+    if char not in word:
+        counter-=1 
+    if counter == 0:
+        print('loooossser ')
+        break           
     if ar == word:
         print('you are Winner ')
         break    
